ba0f7a8bc5fea70d26bdf4fc12f1c81c76c23700
Abdulwahab <wahab210899@gmail.com>
2023-11-07 19:52:38 +0300

    GOING TO MAKE THE HOME PAGE

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { CustomNav } from "./components/custom_nav.jsx";
-import {Auth} from './pages/auth.jsx';
+import { Auth } from "./pages/auth.jsx";
+import { Home } from "./pages/home.jsx";
 function App() {
   return (
     <div className="App">
       <Router>
         <CustomNav />
         <Routes>
-          <Route path="/"/>
-          <Route path="/auth" element = {<Auth />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
         </Routes>
       </Router>
     </div>
---
+++ client/src/pages/home.jsx
@@ -0,0 +1,17 @@
+import '../styles/home.css'
+export const Home = () => {
+  return (
+    <div className="home-wrapper">
+      <div className="left-section">
+        <h1>Welcome to Dallani</h1>
+        <p>
+          Dallani is a website that helps students find co-op and summer
+          training opportunity in different companies.
+        </p>
+      </div>
+      <div className="right-section">
+        
+      </div>
+    </div>
+  );
+};
