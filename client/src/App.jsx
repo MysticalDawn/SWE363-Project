6d0a0a4ff1d4d6b389e75cc91170cff558289ed6
Abdulwahab <wahab210899@gmail.com>
2023-11-08 20:57:51 +0300

    DELETED AUTH PAGE. MADE SOME MODIFICATIONS. THREE

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { CustomNav } from "./components/custom_nav.jsx";
-import { Auth } from "./pages/auth.jsx";
+import { Signup } from "./pages/signup.jsx";
 import { Home } from "./pages/home.jsx";
+import { Catalog } from "./pages/catalog.jsx";
 function App() {
   return (
     <div className="App">
@@ -9,7 +10,8 @@ function App() {
         <CustomNav />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/auth" element={<Auth />} />
+          <Route path="/auth" element={<Signup />} />
+          <Route path="/catalog" element={<Catalog />} />
         </Routes>
       </Router>
     </div>
--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,16 +1,34 @@
 import '../styles/home.css'
+import {useState, useEffect} from 'react'
+import {Link} from 'react-router-dom'
 export const Home = () => {
+  const [text, setText] = useState("")
+  const [fullText] = useState(
+    "Welcome to Dallani 🙂"
+  )
+  const [index, setIndex] = useState(0)
+  useEffect(() => {
+    if(index < fullText.length) {
+      setTimeout(() => {
+        setText(text + fullText[index])
+        setIndex(index + 1)
+      }, 60)
+    }
+  }, [fullText, index, text])
   return (
     <div className="home-wrapper">
       <div className="left-section">
-        <h1>Welcome to Dallani</h1>
-        <p>
+        <h1>{text}</h1>
+        <p className='info'>
           Dallani is a website that helps students find co-op and summer
           training opportunity in different companies.
         </p>
+        <Link to="/catalog">
+          <button className="browse-btn">Browse our catalog</button>
+        </Link>
       </div>
       <div className="right-section">
-        
+        <canvas id = "canvas"></canvas>
       </div>
     </div>
   );
