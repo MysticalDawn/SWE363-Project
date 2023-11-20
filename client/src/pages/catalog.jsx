94c67f94e182aaf28030ca6fcec2a110a3b770c8
Ahmad alqarni <aalqarnimr@gmail.com>
2023-11-21 01:28:07 +0300

    job card component and responsiveness

--- client/src/pages/catalog.jsx
+++ client/src/pages/catalog.jsx
@@ -1,17 +1,52 @@
 import { CustomNav } from "../components/custom_nav.jsx";
 import "../styles/catalog.css";
 import aramco from "../img/aramco-logo.png";
-import Rating from '@mui/material/Rating';
+import Rating from "@mui/material/Rating";
 import locationLogo from "../img/location.svg";
 export const Catalog = () => {
-  let majors =["CS","SWE","COE","EE"];
-  function majorsElement(majorsList){
-    let elementsList = majorsList.map(major=><i className={"major "+major} key={major}>
-      {major}
-    </i>)
-    return <div className="targetted-majors">
-      {elementsList}
-    </div>
+  let majors = ["CS", "SWE", "COE", "EE"];
+  function majorsElement(majorsList) {
+    let elementsList = majorsList.map((major) => (
+      <i className={"major " + major} key={major}>
+        {major}
+      </i>
+    ));
+    return <div className="targetted-majors">{elementsList}</div>;
+  }
+  let aramcoJob = {
+    company: "Aramco",
+    rating_score: 4.5,
+    rating_count: 319,
+    compLocation: "Dhahran",
+    targetMajors: majors,
+  };
+  function cardElement(jobObject) {
+    return (
+      <article className="job-card">
+        <section className="card-body">
+          <img src={aramco} alt="logo" width={40} />
+          <h1 className="company-name">{jobObject.company}</h1>
+          <p className="rating">
+            <Rating
+              className="stars"
+              value={jobObject.rating_score}
+              readOnly
+            ></Rating>
+            <i className="rating-count">({jobObject.rating_count} users)</i>
+          </p>
+          <div className="train-location">
+            <img src={locationLogo} alt="location" width={20} />
+            <h2>{jobObject.compLocation}</h2>
+          </div>
+          {majorsElement(jobObject.targetMajors)}
+        </section>
+        <section className="outer-card">
+          <button type="submit" className="apply">
+            Apply
+          </button>
+        </section>
+      </article>
+    );
   }
   return (
     <>
@@ -55,23 +90,28 @@ export const Catalog = () => {
           <section id="jobs-grid">
             <article className="job-card">
               <section className="card-body">
-                <img src={aramco} alt="logo" width={40}/>
+                <img src={aramco} alt="logo" width={40} />
                 <h1 className="company-name">Aramco</h1>
                 <p className="rating">
                   <Rating className="stars" value={4} readOnly></Rating>
                   <i className="rating-count">(318 users)</i>
                 </p>
                 <div className="train-location">
-                  <img src={locationLogo} alt="location" width={20}/>
+                  <img src={locationLogo} alt="location" width={20} />
                   <h2>Dhahran</h2>
                 </div>
                 {majorsElement(majors)}
               </section>
               <section className="outer-card">
-                
-                <button type="submit" className="apply">Apply</button>
+                <button type="submit" className="apply">
+                  Apply
+                </button>
               </section>
             </article>
+            {cardElement(aramcoJob)}
+            {cardElement(aramcoJob)}
+            {cardElement(aramcoJob)}
+            {cardElement(aramcoJob)}
           </section>
         </section>
       </main>
