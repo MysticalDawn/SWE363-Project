c34eccfb33108e9f5430e42bc2ff8bd320a2f3ee
Abdulwahab <wahab210899@gmail.com>
2023-12-05 22:08:26 +0300

    Made changes to confirmation page.

--- client/src/components/confirmation.jsx
+++ client/src/components/confirmation.jsx
@@ -1,15 +1,23 @@
 import Success from "../img/success.svg";
+import "../styles/confirmation.css";
+import { Link } from "react-router-dom";
 export const ConfirmationPage = () => {
   return (
     <div className="confirm-wrapper">
-      <img
-        src={Success}
-        alt="success"
-        height="200"
-        width="200"
-        className="success-img"
-      />
-      <h1 className="confirm-header">Success!</h1>
+      <div className="confirm-content">
+        <img
+          src={Success}
+          alt="success"
+          height="200"
+          width="200"
+          className="success-img"
+        />
+        <h1 className="confirm-header">Success!</h1>
+        <p className="confirm-text">You process has been completed!</p>
+        <Link to="/" className="conf browse-btn" id="confirm-btn">
+          Go back
+        </Link>
+      </div>
     </div>
   );
 };
