6cdc261180b4659f003073c519ff19a79b1dd049
OmarAlfawaz <omar12345alfawaz@gmail.com>
2023-12-08 17:49:18 +0300

    small modification

--- server/src/routes/GetUserInfo.js
+++ server/src/routes/GetUserInfo.js
@@ -4,28 +4,28 @@ import mongoose from "mongoose";
 const router = express.Router();
 
 router.get("/", async (req,res)=> {
-   const token = req.headers['authorization']?.split(' ')[1];
-   if (!token) {
-     return res.status(401).json({ error: 'Unauthorized' });
-   }
-
-   try {
-     jwt.verify(token, process.env.JWT_SECRET);
-   } catch (error) {
-     return res.status(401).json({ error: 'Invalid token' });
-   }
-
-   const userId = jwt.decode(token).id;
-   const user = await mongoose.model("User").findOne({_id: userId});
-
-   if (!user) {
-     return res.status(404).json({ error: 'User not found' });
-   }
-
-   // Remove the password from the user object before sending it in the response
-   delete user.password;
-
-   return res.json(user);
-})
+    const token = req.headers['authorization']?.split(' ')[1];
+    if (!token) {
+      return res.status(401).json({ error: 'No token provided' });
+    }
+   
+    try {
+      jwt.verify(token, 'secret'); // Use the same secret key that was used in the jwt.sign function
+    } catch (error) {
+      return res.status(401).json({ error: 'Invalid token' });
+    }
+   
+    const userId = jwt.decode(token).id;
+    const user = await mongoose.model("User").findOne({_id: userId});
+   
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+   
+    delete user.password;
+   
+    return res.json(user);
+   })
+   
 
 export {router as GetUserInfo}
