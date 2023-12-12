d1affa6463b6226ec3cd672d8a12946d32a11ee1
HamadSadran <hamad.ib.sadran@gmail.com>
2023-12-12 09:24:35 +0300

    Added handleSubmit to forgot page

--- client/src/components/forgot.jsx
+++ client/src/components/forgot.jsx
@@ -1,22 +1,55 @@
-import Success from "../img/success.svg";
 import "../styles/forget.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 export const ForgotPassword = () => {
+  const [email, setEmail] = useState("");
+	const [msg, setMsg] = useState("");
+	const [error, setError] = useState("");
+
+  const handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			const url = `http://localhost:5173/password-reset`;
+			const { data } = await axios.post(url, { email });
+			setMsg(data.message);
+			setError("");
+		} catch (error) {
+			if (
+				error.response &&
+				error.response.status >= 400 &&
+				error.response.status <= 500
+			) {
+				setError(error.response.data.message);
+				setMsg("");
+			}
+		}
+	};
+
   return (
     <div className="forget-wrapper">
       <div className="forget-content">
-        <img
-          src={Success}
-          alt="success"
-          height="200"
-          width="200"
-          className="success-img"
-        />
-        <h1 className="forget-header">Enter your email</h1>
-        <input className="email-input" type="text" placeholder="someone@email.com" />
-        <Link to="/" className="conf browse-btn" id="forgot-btn">
+        <form onSubmit={handleSubmit}>
+          <h1 className="forget-header">Enter your email</h1>
+          <input
+					type="email"
+					placeholder="someone@email.com"
+					name="email"
+					onChange={(e) => setEmail(e.target.value)}
+					value={email}
+					required
+					className="email-input"
+          />
+          {error && <div className="error_msg">{error}</div>}
+          {msg && <div className="success_msg">{msg}</div>}
+          <br />
+          <button type="submit" className="conf browse-btn" id="forgot-btn">
+            Submit
+          </button>
+        </form>
+        {/* <Link to="/" className="conf browse-btn" id="forgot-btn">
           Submit
-        </Link>
+        </Link> */}
       </div>
     </div>
   );
